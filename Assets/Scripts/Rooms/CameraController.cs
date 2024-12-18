ca91d06179a93070be01c72154f53cd9293c8e28
marioneta04 <marianofanelli03@gmail.com>
2024-12-18 17:18:21 -0300

    Update CameraController.cs

--- Assets/Scripts/Rooms/CameraController.cs
+++ Assets/Scripts/Rooms/CameraController.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    //Room camera
+    
     [SerializeField] private float speed;
     private float currentPosX;
     private Vector3 velocity = Vector3.zero;
 
-    //Follow player
+    
     [SerializeField] private Transform player;
     [SerializeField] private float aheadDistance;
     [SerializeField] private float cameraSpeed;
@@ -17,10 +17,10 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
-        //Room camera
+        
         transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
 
-        //Follow player
+        
         transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
         lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
     }
